d4fc240c11b97a12d5134be38e78d2d678f0b480
girkarkshitij <girkarkshitij@gmail.com>
2020-10-09 19:21:23 +0530

    Create basic structure of page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 class App extends React.Component {
     render() {
-        return(
-            <h1>Hello</h1>
-        )
+        return (
+            <div className='container'>
+                <div id='quote-box'>
+                    <div id='text'>Quote Text</div>
+                    <div id='author'>Quote Author</div>
+                    <a href='twitter.com/intent/tweet' id='tweet-quote'>
+                        Tweet Quote
+                    </a>
+                    <button id='new-quote'>New Quote</button>
+                </div>
+            </div>
+        );
     }
 }
 
