8c0f0b18314f3cd941d2671c2b3a436f86b6b4e7
girkarkshitij <girkarkshitij@gmail.com>
2020-10-09 19:53:47 +0530

    Add state and fetchQuote method to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            quote: 'If you can dream it, you can achieve it.',
+            author: 'Zig Ziglar',
+        };
+        this.fetchQuote = this.fetchQuote.bind(this);
+    }
+
+    fetchQuote() {
+        fetch(
+            'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+        )
+            .then((data) => data.json())
+            .then((quotesArray) => {
+                const newQuote =
+                    quotesArray.quotes[Math.floor(Math.random() * 101)];
+                this.setState({
+                    quote: newQuote['quote'],
+                    author: newQuote['author'],
+                });
+            });
+    }
+
     render() {
         return (
             <div className='container'>
                 <div id='quote-box'>
-                    <div id='text'>Quote Text</div>
-                    <div id='author'>Quote Author</div>
+                    <div id='text'>{this.state.quote}</div>
+                    <div id='author'>{this.state.author}</div>
                     <a href='twitter.com/intent/tweet' id='tweet-quote'>
                         Tweet Quote
                     </a>
-                    <button id='new-quote'>New Quote</button>
+                    <button id='new-quote' onClick={this.fetchQuote}>
+                        New Quote
+                    </button>
                 </div>
             </div>
         );
