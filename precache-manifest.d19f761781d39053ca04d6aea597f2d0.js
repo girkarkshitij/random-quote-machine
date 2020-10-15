06df173f71c8e0d4521a4dbbc85a4b2cb8862289
girkarkshitij <girkarkshitij@gmail.com>
2020-10-15 13:11:16 +0530

    Updates

---
+++ precache-manifest.d19f761781d39053ca04d6aea597f2d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b21e9dacdbabae216797ed74fa194be",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "e2a42ba04cf8481e0176",
+    "url": "/random-quote-machine/static/css/main.f21e1070.chunk.css"
+  },
+  {
+    "revision": "2384b7aafcdc87349cdc",
+    "url": "/random-quote-machine/static/js/2.8a21fb19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.8a21fb19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a42ba04cf8481e0176",
+    "url": "/random-quote-machine/static/js/main.a790b86c.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
